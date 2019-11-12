73519a8815879b9983fa230fc6fd0c3b571361d0
박수지 <charmmming7@gmail.com>
2019-11-12 19:16:25 +0900

    컴포넌트 만들기2 (리팩토링)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
+// App.js 는 자바스크립트가 아니다. 유사 자바스크립트.
 /* Class Type */
 import React, { Component } from 'react';
 import './App.css';
+
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header> 
+    );
+  }
+}
+
+class Nav extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JAVASCRIPT</a></li>
+        </ul>  
+      </nav> 
+    );
+  }
+}
+
+class Article extends Component{
+  render(){
+    return(
+      <article>
+        <h2>HTML</h2>
+        HTML is ~~~~.
+      </article>
+    );
+  }
+}
 
 //컴포넌트 만들기
 class App extends Component{
   render() {
     return(
       <div className="App">
-        <header>
-          <h1>WEB</h1>
-          world wide web!
-        </header> 
+        <Subject></Subject>
+        <Nav></Nav>
+        <Article></Article>
       </div>  
     );
   }
