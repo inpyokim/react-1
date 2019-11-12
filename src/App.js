c5c37fbc25cda381773bc2ae0d3702a8768fd79c
박수지 <charmmming7@gmail.com>
2019-11-12 20:09:39 +0900

    state 활용(+key)

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-// App.js 는 자바스크립트가 아니다. 유사 자바스크립트.
 /* Class Type */
 import React, { Component } from 'react';
+import Nav from './components/Nav';
+import Article from './components/Article'
+import Subject from './components/Subject'
 import './App.css';
-
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-      <h1>{this.props.title}</h1>
-      {this.props.sub}
-      </header> 
-    );
-  }
-}
-
-class Nav extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JAVASCRIPT</a></li>
-        </ul>  
-      </nav> 
-    );
-  }
-}
-
-class Article extends Component{
-  render(){
-    return(
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
 
 //컴포넌트 만들기
 class App extends Component{
-  // 컴포넌트가 실행될때 constructor가 있으면 제일 먼저 실행되서, 초기화를 담당한다.
+  // 먼저 State 값 초기화
   constructor(props){
     super(props);
     this.state = {
@@ -62,24 +28,10 @@ class App extends Component{
           title={this.state.subject.title} 
           sub={this.state.subject.sub}>
         </Subject>
-        <Nav></Nav>
+        <Nav data={this.state.nav}></Nav>
         <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
       </div>  
     );
   }
 }
 export default App;
-
-/* Function Type
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-function App() {
-  return (
-    <div className="App">
-      hello@!!
-    </div>
-  );
-}
-export default App;
-*/
